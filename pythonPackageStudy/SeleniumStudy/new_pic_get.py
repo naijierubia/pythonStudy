f77ba851560e4f416ba0839772d9de2784fc382b
Naijie Rubia <sakaidohibiki@gmail.com>
2024-03-19 00:51:34 +0800

    fix Bug

--- pythonPackageStudy/SeleniumStudy/new_pic_get.py
+++ pythonPackageStudy/SeleniumStudy/new_pic_get.py
@@ -1,21 +1,35 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import requests
+import os
+import time
 
 driver = webdriver.Chrome()
-driver.implicitly_wait(10)
+driver.implicitly_wait(20)
 driver.get("https://wiki.biligame.com/blhx/%E5%BD%B1%E7%94%BB%E7%9B%B8%E5%85%B3")
 
 elements = driver.find_elements(By.CSS_SELECTOR, "#mw-content-text > div > center > ul > li")
+index = False
 for item in elements:
     item.find_element(By.TAG_NAME,"a").click()
     button = driver.find_element(By.CSS_SELECTOR,"a.mw-mmv-download-button")
+    if index is False:
+        time.sleep(0.2)
+        index = True
     link = button.get_attribute("href")
     print(link)
-    pic = requests.get(link)
-    with open("./azure_lane_pic/" + link.split("/")[-1], "wb")as file:
+    pic =  requests.get(link)
+    with open(os.path.join(".\\azure_lane_pic", link.split("/")[-1]), "wb")as file:
         file.write(pic.content)
     driver.back()
 
+top = driver.find_element(By.CSS_SELECTOR,"#mw-content-text > div > center > div > div")
+top.find_element(By.TAG_NAME,"a").click()
+button = driver.find_element(By.CSS_SELECTOR,"a.mw-mmv-download-button")
+link = button.get_attribute("href")
+print(link)
+pic = requests.get(link)
+with open(os.path.join(".\\azure_lane_pic", link.split("/")[-1]), "wb")as file:
+    file.write(pic.content)
 driver.quit()
 
